a48f7f6611380585c748c8b98c44908e6c949fd8
github username <github email address>
2020-11-23 13:24:23 -0500

    Done

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+
+const bodyCont = document.querySelector('body')
+const mainCont = bodyCont.querySelector('main').remove()
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.textContent = 'Israel is the champion'
+bodyCont.append(newHeader)
+
+
